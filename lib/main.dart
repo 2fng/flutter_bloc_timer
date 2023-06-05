43a2d0d9eed02d5cc9f253f9f82c2fdce61c6c7e
tunghs-1253 <hua.son.tung@sun-asterisk.com>
2023-06-05 15:48:56 +0700

    Finish Timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_timer_bloc/app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  runApp(const App());
 }
